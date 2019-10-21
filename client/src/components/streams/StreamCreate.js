cbbee97880c3bc05cb0d6bd746c8ec98c5d2d2d4
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2019-10-21 23:52:39 +0530

    rdux form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-function StreamCreate() {
-    return (
-        <div>StreamCreate</div>
-    )
+class StreamCreate extends React.Component {
+    
+    //extracting { input } from formProps
+    renderInput({ input, label }) {
+        // return (
+        //         <input 
+        //             value={formProps.input.value}
+        //             onChange={formProps.input.onChange}
+        //         />
+        // )
+
+        // OR
+        return (
+            <div className='form-group'>
+                <label for='email'>{label}</label>
+                <input className='form-control' {...input}/>
+            </div> 
+        )
+    }
+
+    render( ) {
+        return (
+            <form>
+                <Field name='title' component={this.renderInput} label='Enter Title: '/>
+                <Field name='description' component={this.renderInput} label='Enter Description: '/>
+            </form>
+        )
+    }
+    
 }
 
-export default StreamCreate
+export default reduxForm({
+    form: 'StreamCreate'
+})(StreamCreate)
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
 import reducers from './reducers'
 import App from './components/App'
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+    reducers,
+    composeEnhancer(applyMiddleware())
+    ) 
+
+
 ReactDOM.render (
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector('#root')
